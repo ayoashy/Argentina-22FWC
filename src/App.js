b922274aa6529fe87503bdf00b973106212072a4
Topboyshoks <ashiruayo@gmail.com>
2023-01-02 00:57:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import './styles/style.css';
+import { pictures } from './data';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import { BsDot } from 'react-icons/bs';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [isMoving, setIsmoving] = useState(true);
+  const lastSlide = pictures.length - 1;
+
+  // const nextSlide = () => {
+  //   setCounter(counter + 1);
+  // };
+  // const prevSlide = () => {
+  //   if (counter < 0) {
+  //     setCounter(8);
+  //   }
+  //   setCounter(counter - 1);
+  // };
+
+  const nextSlide = () => {
+    setCounter((counter) => {
+      return counter + 1;
+    });
+  };
+  const prevSlide = () => {
+    const lastSlide = pictures.length - 1;
+    // if (counter < 0) {
+    //   setCounter(lastSlide);
+    // }
+    setCounter((counter) => {
+      return counter - 1;
+    });
+  };
+
+  useEffect(() => {
+    if (counter < 0) {
+      setCounter(lastSlide);
+    }
+    if (counter > lastSlide) {
+      setCounter(0);
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    let interval;
+    if (isMoving) {
+      interval = setInterval(() => {
+        setCounter(counter + 1);
+      }, 1500);
+    }
+    return () => clearInterval(interval);
+  }, [counter, isMoving]);
+
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>Argentina</h1>
+          <ul>
+            {[1, 2, 3, 4, 5].map((vamos, index) => (
+              <li key={index}>vamos</li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+
+      <section>
+        <article>
+          <img
+            src={pictures[counter]}
+            alt='vamos argentina'
+            onMouseOver={() => setIsmoving(false)}
+            onMouseOut={() => setIsmoving(true)}
+          />
+          <button className='btn1' onClick={prevSlide}>
+            <AiOutlineArrowLeft />
+          </button>
+          <button className='btn2' onClick={nextSlide}>
+            <AiOutlineArrowRight />
+          </button>
+        </article>
+        <div>
+          {pictures.map((picture, index) => (
+            <button
+              key={index}
+              className={index === counter ? 'scale' : ''}
+              onClick={() => setCounter(index)}
+            >
+              <BsDot size={30} />
+            </button>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const pictures = [
+  'https://www.aljazeera.com/wp-content/uploads/2022/12/SSS15746.jpg?fit=1170%2C780',
+  'https://www.aljazeera.com/wp-content/uploads/2022/12/SSS19776_2.jpg?fit=1170%2C780',
+  'https://www.aljazeera.com/wp-content/uploads/2022/12/SSS18990.jpg?fit=1170%2C780',
+  'https://media.npr.org/assets/img/2022/12/19/gettyimages-1450300260_custom-c5af9bcf4c0a466c8d8bf155c5917cc4ee5b1d62-s2600-c85.webp',
+  'https://media.npr.org/assets/img/2022/12/19/getty-diptych-fans_custom-9e36833c66c8bf1af3327c8ec5d6b87202527d2f-s2600-c85.webp',
+  'https://media.npr.org/assets/img/2022/12/19/ap22352685043814_custom-4c1bee125fe9b3ebc586425dc12fb378eadc8f97-s2600-c85.webp',
+  'https://media.npr.org/assets/img/2022/12/19/gettyimages-1245717752_custom-a4b32e88d04cb49b8d8fb1f888ee931e35da4398-s2600-c85.webp',
+  'https://i.guim.co.uk/img/media/e6ea9dbd54f83af9fb41c6356ef408ee4ee89e1f/0_0_3500_2305/master/3500.jpg?width=1010&quality=45&auto=format&fit=max&dpr=2&s=1b779f6565900f98f4a707c0d91d3eec',
+  'https://i.guim.co.uk/img/media/200e521df39e1405834637486045d578bb0ba1e0/0_0_3500_2259/master/3500.jpg?width=1010&quality=45&auto=format&fit=max&dpr=2&s=14947a1d48433fb8f06a42fcc5dec46a',
+  'https://images2.minutemediacdn.com/image/upload/c_crop,w_6978,h_3925,x_0,y_40/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/90min_en_international_web/01gmn8cn1xd597qnsc15.jpg',
+
+  'https://media.cnn.com/api/v1/images/stellar/prod/221213213854-argentina-celebrates.jpg?c=original',
+  'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-12/221218-argentina-world-cup-win-jm-1311-12e40d.jpg',
+  'https://news.italy24.press/content/uploads/2022/12/20/37695bfa23.jpg',
+  'https://i.guim.co.uk/img/media/cf9274b3a80826fed2895fba9a49cd54a321225c/0_0_8192_5464/master/8192.jpg?width=1010&quality=45&auto=format&fit=max&dpr=2&s=b7981b2cb0327148481e4214254154d9',
+];
